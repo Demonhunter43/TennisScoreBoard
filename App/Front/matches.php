287864cf6efff18ce7ba0396b5d0a161b0aef739
Vladislav <vladislav.vladvlad2015@yandex.ru>
2024-10-21 15:15:18 +0300

    first commit

---
+++ App/Front/matches.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Сыгранные матчи</title>
+</head>
+<body>
+<h1>Таблица сыгранных матчей</h1>
+<table>
+    <tr>
+        <th>asd</th>
+        <th>asd</th>
+        <th>asd</th>
+    </tr>
+    <tr>
+        <th>7234</th>
+        <th>7234</th>
+        <th>7234</th>
+    </tr>
+
+</table>
+</body>
+</html>
